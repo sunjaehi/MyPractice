d753f499e0c20408a5a46c61605941ba2d90ec85
Lee Sunjae <sunjae7944@naver.com>
2024-04-26 10:46:25 +0900

    Notice - image map

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   
   return (
     <>
-      <Search />
+      <Notice />
       
     </>
   );
--- src/component/page/Notice.js
+++ src/component/page/Notice.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import React from "react";
+import { StaticMap } from "react-kakao-maps-sdk";
 //import { Link } from "react-router-dom";
 
 /*
@@ -10,9 +11,23 @@ const Wrapper=styled.div`
 
 function Notice () {
     return (
-        <div>
-            공지사항                                       
-        </div>
+        <StaticMap
+            center={{
+                lat:33.450701,
+                lng:126.570667
+            }}
+            style={{
+                width:"500px",
+                height:"500px",
+                margin:"10px"
+            }}
+            marker={{
+                lat:37.5029087190,
+                lng:127.0377563750
+            }}
+            level={3}
+            />
+
     );
 }
 export default Notice; 

03977d4613fb922b336d194b0dafadabc9a63532
Lee Sunjae <sunjae7944@naver.com>
2024-04-27 23:28:48 +0900

    Kakao - region

--- src/component/Kakao.js
+++ src/component/Kakao.js
@@ -5,6 +5,10 @@ import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+//import Stack from '@mui/material/Stack';
+import { regionResponse } from '../data/regionResponse';
 
 
 
@@ -16,7 +20,7 @@ function Kakao() {
     return (
         <Box sx={{margin:"10px"}}>
             <FormControl sx={{m:1, minWidth:120}}>
-                <InputLabel id="demo-simple-select-helper-label">거리 선택</InputLabel>
+                <InputLabel id="demo-simple-select-helper-label">검색할 지역구</InputLabel>
                 <Select
                     labelId='demo-simple-select-helper-label'
                     id='demo-dimple-select-helper'
@@ -24,12 +28,17 @@ function Kakao() {
                     label="거리"
                     onChange={handleChange}
                 >
-                    <MenuItem value={300}>300m</MenuItem>
-                    <MenuItem value={500}>500m</MenuItem>
-                    <MenuItem value={1000}>1km</MenuItem>
+                    {regionResponse.map((region)=> (
+                        <MenuItem
+                            id={`${region.id}`}
+                        >{`${region.name}`}</MenuItem>
+                    ))}
                 </Select>
-                <FormHelperText>거리를 설정해주세요</FormHelperText>
+                <FormHelperText>지역을 설정해주세요</FormHelperText>
             </FormControl>
+            <Button sx={{margin:"10px"}} variant='outlined' size="small" endIcon={<SendIcon />}>
+                Send
+            </Button>
         </Box>
     );
 }

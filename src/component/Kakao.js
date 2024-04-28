0938ced0f4d9e505d47714a82230da1bfdb4c591
Lee Sunjae <sunjae7944@naver.com>
2024-04-28 15:57:59 +0900

    handleSubmit

--- src/component/Kakao.js
+++ src/component/Kakao.js
@@ -12,15 +12,21 @@ import { regionResponse } from '../data/regionResponse';
 
 function Kakao() {
     const [distance,setDistance] = useState('');
+    //드롭다운 값이 변경될 때 호출되는 이벤트 핸들러
     const handleChange = (event)=> {
+        //선택된 값을 상태에 업데이트
         setDistance(event.target.value);
-    }
-    
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Selected distance: ",distance);
+    };
+        
     return (
         <Box sx={{margin:"10px"}}>
             <FormControl 
                 sx={{m:1, minWidth:120}}
-                
+                onSubmit={handleSubmit}
             >
                 <InputLabel id="demo-simple-select-helper-label">검색할 지역구</InputLabel>
                 <Select
@@ -44,6 +50,7 @@ function Kakao() {
                 size="small" 
                 endIcon={<SendIcon />}
                 type="submit"
+                onClick={handleSubmit}
             >
                 Send
             </Button>
